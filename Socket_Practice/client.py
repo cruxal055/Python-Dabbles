b8b12aee6c867732e6c6212fc1a10f4f60c92b13
Alan <aslu1@uci.edu>
2019-08-29 22:49:07 -0700

    added chat room

--- Socket_Practice/client.py
+++ Socket_Practice/client.py
@@ -2,12 +2,35 @@ import socket
 
 
 temp = socket.socket()
+temp.connect(("IP_ADDRESS", 1234))
+print("connected!")
 
-temp.connect(("HIDDEN", 1234))
+write_to = socket.socket();
+write_to.bind(('', 1234))
+write_to.listen()
+
+print("connected pt 2")
+
+client, from_where = write_to.accept()
+
+printer = client.makefile('w')
+user_input = ' '
+
+
 print("sucuessfully connected!")
 temp_input = temp.makefile('r')
 str_input = " "
 while len(str_input) != 0:
     str_input = temp_input.readline()
-    print(str_input)
+    print("they said: " + str_input)
+    user_input = input("your response?")
+    printer.write(user_input + "\n")
+    printer.flush()
+
+    # if str_input == ("RESPOND" + '\n'):
+    #     user_input = input("your response?")
+    #     printer.write(user_input + "\n")
+    #     printer.flush()
+    # else:
+    #     print(str_input)
 print("im done")
--- Socket_Practice/server.py
+++ Socket_Practice/server.py
@@ -1,13 +1,14 @@
 import socket
 
 temp = socket.socket()
-# temp.bind((socket.gethostbyname('localhost'), 1234))
-temp.bind((socket.gethostbyname('localhost'), 1234))
-print(socket.gethostbyname('localhost'))
+# temp.bind((socket.gethostname(), 1234))
+temp.bind(('', 1234))
 
 # while True:
+print("looking for a signal")
 temp.listen()
 clientSoc, where = temp.accept()
+print("found a signal from " + str(where))
 printer = clientSoc.makefile('w')
 user_input = " "
 while len(user_input) != 0:
